642b1d1e406ab2c9cdc6e81f9143d336e934f46b
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-02-08 22:22:04 +0700

    define validation function

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -1,8 +1,42 @@
-import {Request} from 'express';
-import Joi,{ValidationResult} from 'joi';
+import { Request } from 'express';
+import Joi, { ValidationResult } from 'joi';
+import { configSchema, finalResponseSchema } from './schema';
 
-// const Joi = require("@hapi/joi");
-export const registerValidation = (data:Request) :ValidationResult => {
+export const storeConfigurevalidation = (data: Request): ValidationResult => {
+  const schema = Joi.object({
+    projectId: Joi.string().required(),
+    description: Joi.string(),
+    config: configSchema
+  })
+  return schema.validate(data)
+}
+
+
+export const storeProjectValidation = (data: Request): ValidationResult => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().allow(null, ''),
+    configures: Joi.array().items(configSchema),
+    finalResponse: finalResponseSchema,
+  })
+  return schema.validate(data)
+}
+
+export const updateProjectValidation = (data: Request): ValidationResult => {
+  const schema = Joi.object({
+    project: Joi.object({
+      id : Joi.string().required(),
+      name: Joi.string().required(),
+      description: Joi.string().allow(null, ''),
+      configures: Joi.array().items(configSchema).required(),
+      finalResponse: finalResponseSchema
+
+    })
+  })
+  return schema.validate(data)
+}
+
+export const registerValidation = (data: Request): ValidationResult => {
   const schema = Joi.object({
     name: Joi.string().min(5).max(255).required(),
     email: Joi.string().min(6).max(255).required().email(),
@@ -12,12 +46,12 @@ export const registerValidation = (data:Request) :ValidationResult => {
 };
 
 
-export const loginValidation = (data:Request) : ValidationResult => {
-    const schema = Joi.object({
-        email: Joi.string().min(6).max(255).required().email(),
-        password: Joi.string().min(6).max(1024).required(),
-      });  
-      return schema.validate(data);
+export const loginValidation = (data: Request): ValidationResult => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required(),
+  });
+  return schema.validate(data);
 }
 
 
