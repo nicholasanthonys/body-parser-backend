32c6cd167dcbd67abfad10049f2b6605ef5577dc
nicholas <anthony.nicholas84@gmail.com>
2021-06-23 20:40:28 +0700

    throw error in when error happened in controller

--- src/modules/ConfigureFile/ConfigureFileController.ts
+++ src/modules/ConfigureFile/ConfigureFileController.ts
@@ -1,25 +1,39 @@
 import { IProject, Project } from "../Project/Project";
 
-export default class ConfigureFileController{
-
-    async deleteConfigureFileParallel(projectId : string, userId : string ,configureFileId : string) : Promise<boolean> {
-        const project = await Project.findOne({_id : projectId, userId}) as IProject;
-        if(project){
-            project.parallel.configures= project.parallel.configures.filter(c => c._id != configureFileId)
-            await project.save()
-            return true;
-        }
-        return false;
+export default class ConfigureFileController {
+  async deleteConfigureFileParallel(
+    projectId: string,
+    userId: string,
+    configureFileId: string
+  ) {
+    const project = (await Project.findOne({
+      _id: projectId,
+      userId,
+    })) as IProject;
+    if (project) {
+      project.parallel.configures = project.parallel.configures.filter(
+        (c) => c._id != configureFileId
+      );
+      await project.save();
     }
+    throw new Error("Project not found");
+  }
 
-    async deleteConfigureFileSerial(projectId : string, userId : string ,configureFileId : string) : Promise<boolean> {
-        const project = await Project.findOne({_id : projectId, userId}) as IProject;
-        if(project){
-            project.serial.configures= project.serial.configures.filter(c => c._id != configureFileId)
-            await project.save()
-            return true;
-        }
-        return false;
+  async deleteConfigureFileSerial(
+    projectId: string,
+    userId: string,
+    configureFileId: string
+  ) {
+    const project = (await Project.findOne({
+      _id: projectId,
+      userId,
+    })) as IProject;
+    if (project) {
+      project.serial.configures = project.serial.configures.filter(
+        (c) => c._id != configureFileId
+      );
+      await project.save();
     }
-
+    throw new Error("Project not found");
+  }
 }
--- src/modules/Project/Controller/ProjectController.ts
+++ src/modules/Project/Controller/ProjectController.ts
@@ -1,73 +1,87 @@
 import { Configure } from "src/modules/Configure/Configure";
 import { IProject, Project } from "../Project";
-import IStoreProjectDTO from '../DTO/StoreProjectDTO'
-import IUpdateProjectDTO from '../DTO/UpdateProjectDTO'
+import IStoreProjectDTO from "../DTO/StoreProjectDTO";
+import IUpdateProjectDTO from "../DTO/UpdateProjectDTO";
 
 export default class ProjectController {
+  async store(
+    storeProjectDTO: IStoreProjectDTO,
+    userId: string
+  ): Promise<IProject> {
+    return await new Project({
+      userId,
+      name: storeProjectDTO.name,
+      description: storeProjectDTO.description,
+      base: storeProjectDTO.base,
+    }).save({
+      //* Set check keys = false in order to insert key with ($) or (.)
+      checkKeys: false,
+    });
+  }
 
-    async store(storeProjectDTO: IStoreProjectDTO, userId: string): Promise<IProject> {
-        return await new Project({
+  async getAll(userId: string) {
+    //* Get project without its configures, and sort by date (newest or descending)
+    return await Project.find({ userId })
+      .sort({ date: "desc" })
+      .select(["-configures", "-serial", "-parallel"]);
+  }
 
-            userId,
-            name: storeProjectDTO.name,
-            description: storeProjectDTO.description,
-            base: storeProjectDTO.base
-        }).save({
-            //* Set check keys = false in order to insert key with ($) or (.)
-            checkKeys: false,
-        });
-
+  async show(projectId: string, userId: string): Promise<IProject> {
+    const project = await Project.findById({
+      _id: projectId,
+      userId,
+    });
+    if (!project) {
+      throw new Error("Project not found");
     }
+    return project;
+  }
 
-    async getAll(userId: string) {
-        //* Get project without its configures, and sort by date (newest or descending)
-        return await Project.find({ userId })
-            .sort({ date: "desc" })
-            .select(["-configures", '-serial', '-parallel' ]);
+  async update(updateProjectDTO: IUpdateProjectDTO) {
+    const project = (await Project.findById(updateProjectDTO.id)) as IProject;
+    if (!project) {
+      throw new Error("Project not found");
     }
+    project.name = updateProjectDTO.name;
+    project.description = updateProjectDTO.description;
+    project.base.project_max_circular =
+      updateProjectDTO.base.project_max_circular;
+    project.base.circular_response.status_code =
+      updateProjectDTO.base.circular_response.status_code;
 
-    async show(projectId: string, userId : string): Promise<IProject> {
-        return await Project.findById({
-            _id : projectId,
-            userId
-        });
+    project.base.circular_response.log_before_modify =
+      updateProjectDTO.base.circular_response.log_before_modify;
+    project.base.circular_response.log_after_modify =
+      updateProjectDTO.base.circular_response.log_after_modify;
+
+    project.base.circular_response.adds.header =
+      updateProjectDTO.base.circular_response.adds.header;
+
+    project.base.circular_response.adds.header =
+      updateProjectDTO.base.circular_response.adds.header;
+    project.base.circular_response.adds.body =
+      updateProjectDTO.base.circular_response.adds.body;
+
+    project.base.circular_response.modifies.header =
+      updateProjectDTO.base.circular_response.modifies.header;
+    project.base.circular_response.modifies.body =
+      updateProjectDTO.base.circular_response.modifies.body;
+
+    project.base.circular_response.deletes.header =
+      updateProjectDTO.base.circular_response.deletes.header;
+    project.base.circular_response.deletes.body =
+      updateProjectDTO.base.circular_response.deletes.body;
+
+    return await project.save();
+  }
+
+  async delete(id: string, userId: string) {
+    const project = await Project.findOne({ _id: id, userId });
+    if (project) {
+      await Project.deleteOne({ _id: id, userId });
+      //* Delete related configures
+      await Configure.deleteMany({ projectId: project._id });
     }
-
-    async update(updateProjectDTO: IUpdateProjectDTO) {
-        const project = await Project.findById(updateProjectDTO.id) as IProject
-        project.name = updateProjectDTO.name
-        project.description = updateProjectDTO.description
-        project.base.project_max_circular = updateProjectDTO.base.project_max_circular
-        project.base.circular_response.status_code = updateProjectDTO.base.circular_response.status_code
-
-        project.base.circular_response.log_before_modify = updateProjectDTO.base.circular_response.log_before_modify
-        project.base.circular_response.log_after_modify = updateProjectDTO.base.circular_response.log_after_modify
-
-        project.base.circular_response.adds.header = updateProjectDTO.base.circular_response.adds.header
-
-        project.base.circular_response.adds.header = updateProjectDTO.base.circular_response.adds.header
-        project.base.circular_response.adds.body = updateProjectDTO.base.circular_response.adds.body
-
-        project.base.circular_response.modifies.header = updateProjectDTO.base.circular_response.modifies.header
-        project.base.circular_response.modifies.body = updateProjectDTO.base.circular_response.modifies.body
-
-        project.base.circular_response.deletes.header=   updateProjectDTO.base.circular_response.deletes.header
-        project.base.circular_response.deletes.body = updateProjectDTO.base.circular_response.deletes.body
-
-
-        return await project.save()
-
-    }
-
-    async delete(id: string, userId: string): Promise<boolean> {
-        const project = await Project.findOne({_id : id , userId});
-        if (project) {
-            await Project.deleteOne({ _id : id, userId});
-
-            //* Delete related configures
-            await Configure.deleteMany({ projectId : project._id});
-            return true
-        }
-        return false
-    }
+    throw new Error("Project not found");
+  }
 }
