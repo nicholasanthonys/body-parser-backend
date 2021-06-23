85741917bf8a034762f717bc25a293cc02227c3c
nicholas <anthony.nicholas84@gmail.com>
2021-06-23 19:52:04 +0700

    use Exception Class HttpException

--- src/middlewares/validate-token.ts
+++ src/middlewares/validate-token.ts
@@ -1,24 +1,21 @@
-import { Request, Response, NextFunction } from 'express';
-import jsonwebtoken from 'jsonwebtoken';
-import decodeToken from '../utils/decodeToken';
+import { Request, Response, NextFunction } from "express";
+import HttpException from "src/exceptions/HttpException";
+import decodeToken from "../utils/decodeToken";
 
 //* middleware to validate token
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     //* Decode the json web token, usually contain user payload
-    const decoded = decodeToken(req)
+    const decoded = decodeToken(req);
     if (decoded) {
       next(); // to continue the flow
     } else {
-      return res.status(403).json({ error: "Token is not valid" });
-
+      next(new HttpException(403, "Token is not valid"));
     }
-
   } else {
-    return res.status(401).json({ error: "Access denied" });
-
-  };
+    next(new HttpException(401, "Access denied"));
+  }
 };
 
 export default validateToken;
